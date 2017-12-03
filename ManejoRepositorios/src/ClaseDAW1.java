bf4d2f21909bcad22b7facfc030c635be6618f99
luisdeblas <luis_de_blas@hotmail.com>
2017-12-03 11:30:11 +0100

    ClaseDAW1.java

---
+++ ManejoRepositorios/src/ClaseDAW1.java
@@ -0,0 +1,33 @@
+
+public class ClaseDAW1 {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		String profesor = "Luis de Blas";
+		String al01_AdrianPosadas ="";
+		String al02_SergioArrey="";
+		String al03_AndreiAvasiloaiei ="";
+		String al04_AlexBerodia = "";
+		String al05_CristinaBlanco = "";
+		String al06_BarbaraBolado = "";
+		String al07_IvanCasado = "";
+		String al08_RubenCayon = "";
+		String al09_PedroDelgado = "";
+		String al10_DarioEscobar = "";
+		String al11_PaulaEstrada = "";
+		String al13_ManuelGarcia = "";
+		String al14_MarioGomez = "";
+		String al15_ManuelGonzalez = "";
+		String al16_CeciliaHerrero = "";
+		String al18_AlfredoMarcano = "";
+		String al20_OmarNieto = "";
+		String al21_RaquelPerez = "";
+		String al22_VictorPernia = "";
+		String al23_JavierRubio = "";
+		String al25_CristianVartic = "";
+		String al26_AlvaroDiez = "";
+
+
+	}
+
+}
